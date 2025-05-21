ac6c1878191da84cfa6f250b7e99e4260412f086
AppeiYA <appeimisic@gmail.com>
2025-05-21 11:22:20 +0100

    changes

--- models/stageBII.model.js
+++ models/stageBII.model.js
@@ -2,6 +2,11 @@ const db = require("../config/mysql2");
 
 class FormStageBII {
   static async create(data, formStageAId, userId) {
+    const {waterCut} = data;
+    let parsedWaterCut;
+    if(waterCut === "true"){
+      parsedWaterCut = true
+    }
     const [result] = await db.execute(
       `INSERT INTO form_stage_b_II (
             formStageAId, depthInstalled, dischargingRate, waterCut, waterCutDate, waterCutTime, staticWaterLevel, userId
@@ -10,7 +15,8 @@ class FormStageBII {
         formStageAId,
         data.depthInstalled,
         data.dischargingRate,
-        data.waterCut,
+        parsedWaterCut,
+        // data.waterCut,
         data.waterCutDate,
         data.waterCutTime,
         data.staticWaterLevel,
